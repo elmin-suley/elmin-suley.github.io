a42ce975d651b1b976c5241e637b558f38518d72
elmin-suley <91567290+elmin-suley@users.noreply.github.com>
2023-07-04 18:26:31 +0100

    Add files via upload
    initial commit

---
+++ src/pages/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <h1>About us</h1>
+    <p>Fullstack developer and a Designer from Algeria, focused on creating modern web apps, passionate about Designe..</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'About us'
+  }
+}
+</script>
